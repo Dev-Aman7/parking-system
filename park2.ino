58dda47101f84dfb3303b66f90d4ad89e49203a2
Aman kumar <32014475+Rockzo77@users.noreply.github.com>
2018-03-11 00:24:50 +0530

    Add files via upload
    parkf code will go in Arduino mega or Arduino connected with ultrasonic
    park2 code will go in Arduino which is controlling barricade.

---
+++ park2.ino
@@ -0,0 +1,39 @@
+#include<Servo.h>
+const int trigPin = 9;
+const int echoPin = 10;
+// defines variables
+long duration;
+int distance;
+Servo my;
+int pos =0;
+void setup() {
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+Serial.begin(9600);
+my.attach(3);// Starts the serial communication
+}
+void loop() {
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+
+if(distance<10)
+{
+  delay(2000);
+  my.write(90);
+  delay(2000);
+  my.write(20);
+  
+}
+}
---
+++ parkf.ino
@@ -0,0 +1,113 @@
+
+#define trigPin1 3
+#define echoPin1 2
+#define trigPin2 5
+#define echoPin2 4
+#define trigPin3 7
+#define echoPin3 6
+#define trigPin4 9
+#define echoPin4 8
+#define trigPin5 11
+#define echoPin5 10
+#define trigPin6 15
+#define echoPin6 14
+#define trigPin7 50
+#define echoPin7 52
+#define l1 22
+#define l2 23
+#define l3 24
+#define l4 25
+#define l5 26
+#define l6 27 
+long duration, distance, s1,s2,s3,s4,s5,s6;
+
+void setup()
+{
+Serial.begin (9600);
+pinMode(trigPin1, OUTPUT);
+pinMode(echoPin1, INPUT);
+pinMode(trigPin2, OUTPUT);
+pinMode(echoPin2, INPUT);
+pinMode(trigPin3, OUTPUT);
+pinMode(echoPin3, INPUT);
+pinMode(trigPin4, OUTPUT);
+pinMode(echoPin4, INPUT);
+pinMode(trigPin5, OUTPUT);
+pinMode(echoPin5, INPUT);
+pinMode(trigPin6, OUTPUT);
+pinMode(echoPin6, INPUT);
+pinMode(l1,OUTPUT);
+pinMode(l2,OUTPUT);
+pinMode(l3,OUTPUT);
+pinMode(l4,OUTPUT);
+pinMode(l5,OUTPUT);
+pinMode(l6,OUTPUT);
+
+}
+void check(int s,int i,int l)
+{
+  if(s>5)
+  {
+    Serial.print("free slot  : "); 
+    Serial.println(i);
+   digitalWrite(l,HIGH);
+    
+  }
+  else
+  {
+   digitalWrite(l,LOW);
+  }
+}
+void loop() {
+SonarSensor(trigPin1, echoPin1);
+s1 = distance;
+check(s1,1,l1);
+SonarSensor(trigPin2, echoPin2);
+s2 = distance;
+check(s2,2,l2);
+SonarSensor(trigPin3, echoPin3);
+s3 = distance;
+check(s3,3,l3);
+SonarSensor(trigPin4, echoPin4);
+s4 = distance;
+check(s4,4,l4);
+SonarSensor(trigPin5, echoPin5);
+s5 = distance;
+check(s5,5,l5);
+SonarSensor(trigPin6, echoPin6);
+s6 = distance;
+check(s6,6,l6 );
+if(s1<5 && s2<5 && s3<5 && s4<5 && s5<5 && s6<5 )
+{
+  Serial.println("No free slot Available.Go to the next row");
+
+  
+}
+Serial.println("------------------------------");
+
+
+//Serial.print(s1);
+//Serial.print(" - ");
+//Serial.print(s2);
+//Serial.print(" - ");
+//Serial.print(s3);
+//Serial.print(" - ");
+//Serial.print(s4);
+//Serial.print(" - ");
+//Serial.print(s5);
+//Serial.print(" - ");
+//Serial.println(s6);
+delay(1000);
+}
+
+void SonarSensor(int trigPin,int echoPin)
+{
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance = (duration/2) / 29.1;
+
+}
